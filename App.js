102313b7503f5ab17b859f7c9ab4e0546de45f07
unknown <leo-crux1997@hotmail.com>
2021-08-19 16:28:43 +0400

    started navigation

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate("Details")}
+            />
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>{
-            <View style={styles.container}>
-                <Text>Test test</Text>
-                <StatusBar style="auto" />
-            </View>
-        }</NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 }
 
