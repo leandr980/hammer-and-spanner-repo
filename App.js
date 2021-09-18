adb7a4783bb9b890078e10c01130839025c0db13
unknown <leo-crux1997@hotmail.com>
2021-09-18 18:04:08 +0400

    testing firebase

--- App.js
+++ App.js
@@ -10,10 +10,28 @@ import { HomeScreen} from './Screens/Home';
 import { Key } from './Screens/Key';
 import { DetailsScreen } from './Screens/Details';
 import { SignatureScreen } from './Screens/Agent';
+import { AdditemScreen } from './Screens/Additem';
 
+import firebase from 'firebase/app'
+// Import the functions you need from the SDKs you need
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAFCkJDDI85c151it10gR_f7Xa-4WFi-YE",
+    authDomain: "realestate-key-tracker.firebaseapp.com",
+    databaseURL: "https://realestate-key-tracker-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "realestate-key-tracker",
+    storageBucket: "realestate-key-tracker.appspot.com",
+    messagingSenderId: "314590691130",
+    appId: "1:314590691130:web:912b6d4290ae29d5c51a03"
+};
 
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +42,7 @@ export default function App() {
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Details" component={DetailsScreen} />
                 <Stack.Screen name="Signature" component={SignatureScreen} />
+                <Stack.Screen name="Additem" component={AdditemScreen} />
 
             </Stack.Navigator>
         </NavigationContainer>
